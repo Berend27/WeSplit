c58286ec47923091714f01f07af7da4850c329b2
Dexter Berend <dexterjberend@gmail.com>
2024-05-16 09:57:40 -0500

    basic functionality

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Dexter Berend on 12/3/23.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
